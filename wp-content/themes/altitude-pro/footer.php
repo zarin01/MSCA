8ed9b0de14f076f14fa84cabdccbf608de6883aa
zarin01 <zarinneugebauer@gmail.com>
2025-01-17 21:42:00 -0700

    Footer Updates

--- wp-content/themes/altitude-pro/footer.php
+++ wp-content/themes/altitude-pro/footer.php
@@ -25,58 +25,68 @@ do_action( 'genesis_before_footer' );
 
 ?>
 <div class="footer-above-parent">
-	<div class="footer-above-widget">
-	    <table>
-			<tr class="footer-above-tr">
-			<div class="nav-links" style="text-align:center;">
-			  <strong>
-			      <a href="/">HOME</a>&nbsp; &nbsp;|&nbsp; &nbsp;
-			      <a href="/who-we-are/">WHO WE ARE</a>&nbsp;&nbsp; |&nbsp; &nbsp;
-			      <a href="/gallery/">GALLERY</a>&nbsp; &nbsp;|&nbsp; &nbsp;
-			      <a href="/news-events/">NEWS &amp; EVENTS</a>&nbsp;&nbsp; |&nbsp; &nbsp;
-			      <a href="/covenants/">COVENANTS</a>&nbsp; &nbsp;|&nbsp; &nbsp;
-			    <a href="/contact-us/"><font size="4">CONTACT US</font></a>
-			  </strong>
-			</div>
+  <div class="footer-above-widget">
 
-	
-	          <td class="footer-above-col-pay">
-	            <div class="footer-above-header"><strong>Buy Membership</strong></></div>
-	            <div id="936165657879808569" align="left" style="width: 100%; overflow-y: hidden;" class="wcustomhtml">
-	              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-	                <input type="hidden" name="cmd" value="_s-xclick">
-	                <input type="hidden" name="hosted_button_id" value="CANQLXU6NXSFN">
-	                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-	                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-	              </form>
-	            </div>
-	          </td>
-	          <td class="footer-above-col" style="width:20.904737602476%; padding:0 15px;">
-	            <div class="footer-above-header"><strong>Email</strong><br></div>
-	            <div class="footer-above-paragraph"><span style="color:rgb(31, 31, 31)"><span>bill.fortresshomeinspection@gmail.com</span></span><br><span style="color:rgb(31, 31, 31)"><span>Send email</span></span></div>
-	          </td>
-	          <td class="footer-above-col" style="width:22.616373696222%; padding:0 15px;">
-	            <div class="footer-above-header"><strong>Facebook</strong></div>
-	            <div class="footer-above-paragraph footer-above-facebook"><a href="http://facebook.com/MountainShadowsCOS" target="_blank">/MountainShadowsCOS</a></div>
-	          </td>
-	          <td class="footer-above-col" style="width:19.939867251807%; padding:0 15px;">
-	            <div class="footer-above-header"><strong>E-Blasts</strong></div>
-	            <div style="text-align:left;">
-	              <div style="height: 10px; overflow: hidden;"></div>
-	              <a class="footer-above-button footer-above-button-small footer-above-button-highlight" href="http://mscaweb.us13.list-manage.com/subscribe?u=475250ae3c4dcf7edc5d66d6d&amp;id=ba2fa1a6c1" target="_blank">
-	                <span class="footer-above-button-inner">SIGN UP</span>
-	              </a>
-	              <div style="height: 10px; overflow: hidden;"></div>
-	            </div>
-	          </td>
-	          <td class="footer-above-col" style="width:20.699859620578%; padding:0 15px;">
-	            <div class="footer-above-header"><em><strong>Mailing Address</strong></em></div>
-	            <div class="footer-above-paragraph">MSCA<br>PO Box 49072<br>Colorado Springs, CO 80949-9072<br></div>
-	          </td>
-	        </tr>
-	    </table>
-	</div>
+    <!-- Navigation Links -->
+    <div class="footer-above-col nav-links text-center">
+      <strong>
+        <a href="/">HOME</a> &nbsp;|&nbsp;
+        <a href="/who-we-are/">WHO WE ARE</a> &nbsp;|&nbsp;
+        <a href="/gallery/">GALLERY</a> &nbsp;|&nbsp;
+        <a href="/news-events/">NEWS &amp; EVENTS</a> &nbsp;|&nbsp;
+        <a href="/covenants/">COVENANTS</a> &nbsp;|&nbsp;
+        <a href="/contact-us/" class="contact-link">CONTACT US</a>
+      </strong>
+    </div>
+
+    <div class="footer-columns">
+
+      <!-- Mailing Address -->
+      <div class="footer-above-col">
+        <div class="footer-above-header"><em><strong>Mailing Address</strong></em></div>
+        <div class="footer-above-paragraph">
+          MSCA<br>
+          PO Box 49072<br>
+          Colorado Springs, CO 80949-9072<br>
+        </div>
+      </div>
+
+      <!-- Buy Membership -->
+      <div class="footer-above-col">
+        <div class="footer-above-header"><strong>Buy Membership</strong></div>
+        <div class="buy-membership">
+          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="CANQLXU6NXSFN">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </form>
+        </div>
+      </div>
+
+      <!-- Email -->
+      <div class="footer-above-col">
+        <div class="footer-above-header"><strong>Email</strong></div>
+        <div class="footer-above-paragraph">
+          <span class="email">bill.fortresshomeinspection@gmail.com</span>
+        </div>
+      </div>
+
+      <!-- E-Blasts -->
+      <div class="footer-above-col">
+        <div class="footer-above-header"><strong>E-Blasts</strong></div>
+        <div class="e-blasts">
+          <a class="footer-above-button footer-above-button-small footer-above-button-highlight" href="http://mscaweb.us13.list-manage.com/subscribe?u=475250ae3c4dcf7edc5d66d6d&amp;id=ba2fa1a6c1" target="_blank">
+            <span class="footer-above-button-inner">SIGN UP</span>
+          </a>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </div>
+
+
 
 
 
